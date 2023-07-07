cf91b00429f07e000f27604bea8f030343dae524
0RyanSouza0 <tlousouza678@gmail.com>
2023-07-06 22:12:22 -0300

    Fazendo Sidebar

---
+++ pag-professor/components/sidebar.php
@@ -0,0 +1,17 @@
+
+<header class="side-bar">
+           
+        <nav>
+        <div class="imagem-logo">
+                <img src="img/trampotec.png" alt="">
+            </div>
+            
+        <ul>
+            <li ><i class="fa-solid fa-thumbs-up fa-lg"></i><a  href="">Indicações</a></li>
+            <li ><i class="fa-solid fa-user fa-lg"></i><a href="">Perfil</a></li>
+            <li><i class="fa-sharp fa-solid fa-bell fa-lg"></i><a href="">Notificações</a></li>
+            <li><i class="fa-solid fa-gear fa-lg"></i><a href="">Configurações</a></li>
+        </ul>
+    </header>
+        </nav>
+
