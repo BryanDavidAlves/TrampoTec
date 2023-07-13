d8b764691ba2569d786f223a1a5d847e7b61c982
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-07-13 19:38:24 -0300

    adicionando a tela de cadastro e modicicando algumas coisas no login

--- pag-aluno/cadastro.php
+++ pag-aluno/cadastro.php
@@ -20,15 +20,27 @@
             <fieldset>
 
                 <img src="img/trampotec-logo.png" alt="">
-                <h1>Login</h1>
+                <h1>Cadastro</h1>
                 <legend><i class="fa-solid fa-user" style="color: #ffffff;"></i></legend>
                 <br>
-
+                <br><br>
+                <div class="inputBox">
+                    <i class="fa-solid fa-id-card" style="color: #ffffff;"></i>
+                    <label class="labelInput" for="nome"> nome:</label>
+                    <input type="text" id="nome" name="nome" class="inputUser" required>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <i class="fa-solid fa-id-card" style="color: #ffffff;"></i>
+                    <label class="labelInput" for="cpf"> CPF:</label>
+                    <input type="text" id="cpf" name="cpf" class="inputUser" required>
+                </div>
                 <br><br>
                 <div class="inputBox">
                     <i class="fa-solid fa-user" style="color: #ffffff;"></i>
                     <label class="labelInput" for="email"> email:</label>
                     <input type="text" id="email" name="email" class="inputUser" required>
+
                 </div>
                 <br><br>
                 <div class="inputBox">
@@ -38,14 +50,22 @@
 
                 </div>
                 <br><br>
+                <div class="inputBox">
+                    <i class="fa-solid fa-lock" style="color: #ffffff;"></i>
+                    <label class="labelInput" for="Rsenha">Repita sua senha:</label>
+                    <input type="password" id="Rsenha" name="senha" class="inputUser" required>
+
+                </div>
+                <br><br>
                 <h2>Esqueceu sua senha?</h2>
                 <div class="botao">
-                    <button class="btn"><a href="index.php" >Entrar</a></button> <!--Tirar o link automatico quando fizer o back-end-->
+                    <button class="btn">Criar</button>
                 </div>
             </fieldset>
         </form>
-        <p>Ainda nao tem cadastro?</p>
-        <a href="cadastro.php"><button class="btn-cadastro">CADASTRAR</button></a>
+
+        <a href="login.php" class="icone"><i id="first" class="fa-solid fa-right-from-bracket"
+                style="color: #ffffff;"></i> VOLTAR</a>
     </div>
     <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
 </body>
