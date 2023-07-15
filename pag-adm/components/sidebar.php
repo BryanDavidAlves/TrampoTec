41fcd91c9e821f58d856e303d6f87a310ce58307
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-07-15 00:32:14 -0300

    adicionando a tela de adm e a side bar da tela de adm

---
+++ pag-adm/components/sidebar.php
@@ -0,0 +1,20 @@
+<aside id="itens">
+    <nav>
+    <img src="../pag-aluno/img/trampotec-logo.png" alt="">
+        <ul>
+            <div class="first">
+                <li> <a href=""><i class="fa-solid fa-table-cells-large" style="color: #3C86D;"></i> Dashboard</a></li>
+                <li><a href="#"><i class="fa-solid fa-building" style="color: #3C86D;"></i> Empresas</a></li>
+                <li><a href="#"><i class="fa-solid fa-chalkboard-user" style="color: #3C86D;"></i> Professores</a></li>
+                <li><a href="#"><i class="fa-solid fa-user" style="color: #3C86D;"></i> Alunos</a></li>
+            </div>
+
+            <div class="second">
+                <li><a href="curriculo.php"><i class="fa-solid fa-bell" style="color: #3C86D;"></i> Notificações</a></li>
+                <li><a href="perfil.php"><i id="config" class="fa-solid fa-gear" style="color: #3C86D;"></i> Configurações</a></li>
+                <li><a href="notificacoes.php"><i id="first" class="fa-solid fa-right-from-bracket"style="color: #3C86D;"></i> Logout</a></li>
+                <li><a href="contato.php"><i class="fa-solid fa-user" style="color: #3C86D;"></i> Perfil</a></li>
+            </div>
+        </ul>
+    </nav>
+</aside>
---
+++ pag-adm/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../reset.css">
+    <link rel="stylesheet" href="../pag-adm/components/component-adm.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include('../pag-adm/components/sidebar.php');
+    ?>
+    <?php
+    include('../pag-adm/components/header.php');
+    ?>
+    <main>
+
+    </main>
+    <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
