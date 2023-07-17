b1c63029c74b1b97dcb575ce81d03074eecb196e
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-07-17 15:21:50 -0300

    adicionando a pagina de notificacoes e o css

--- pag-adm/notificacao.php
+++ pag-adm/notificacao.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../reset.css">
     <link rel="stylesheet" href="components/component-do-adm.css">
-    <link rel="stylesheet" href="css/aluno.css">
+    <link rel="stylesheet" href="css/notificacoes.css">
     <title>Document</title>
 </head>
 
@@ -20,8 +20,21 @@
         <input type="text" placeholder="Pesquisar">
     </header>
     <main>
-        <section class="aluno">
-            
+        <section class="notificacoes">
+            <div class="notificacao" >
+                <i class="fa-solid fa-user-plus"></i>
+                <h2>Há novos professores cadastrados. Veja aqui</h2>
+            </div>
+          
+            <div class="notificacao">
+                <i class="fa-solid fa-user-plus"></i>
+                <h2>Há novas empresas cadastradas. Veja aqui</h2>
+            </div>
+           
+            <div class="notificacao">
+                <i class="fa-solid fa-user-plus"></i>
+                <h2>Há novos aluno cadastrados. Veja aqui</h2>
+            </div>
         </section>
     </main>
     <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
