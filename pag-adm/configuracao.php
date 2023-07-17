269052ae05eb2ea99f7c6c3ce0ad88eaa718f340
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-07-17 15:43:05 -0300

    adicionando a pagina de configuracao e o css

--- pag-adm/configuracao.php
+++ pag-adm/configuracao.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../reset.css">
     <link rel="stylesheet" href="components/component-do-adm.css">
-    <link rel="stylesheet" href="css/aluno.css">
+    <link rel="stylesheet" href="css/configuracao.css">
     <title>Document</title>
 </head>
 
@@ -20,8 +20,40 @@
         <input type="text" placeholder="Pesquisar">
     </header>
     <main>
-        <section class="aluno">
-            
+        <section class="configuracoes">
+            <div class="configuracao">
+                <div class="part1">
+                    <i class="fa-solid fa-user"></i>
+                    <h2> Alterar e-mail</h2>
+                </div>
+                <i class="fa-solid fa-chevron-right"></i>
+            </div>
+
+            <div class="configuracao">
+                <div class="part1">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                   <h2> Alterar nome</h2>
+                </div>
+                <i class="fa-solid fa-chevron-right"></i>
+            </div>
+
+            <div class="configuracao">
+                <div class="part1">
+                    <i class="fa-solid fa-phone"></i>
+                   <h2> Alterar telefone</h2>
+                </div>
+                <i class="fa-solid fa-chevron-right"></i>
+            </div>
+
+            <div class="configuracao">
+                <div class="part1">
+                    <i class="fa-solid fa-lock"></i>
+                   <h2> Alterar senha</h2>
+                </div>
+                <i class="fa-solid fa-chevron-right"></i>
+            </div>
+
+
         </section>
     </main>
     <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
