40028de83c7551b48b88dcf6fe64ee187507132a
Johnydbr <djohny000@gmail.com>
2023-07-22 23:45:09 -0300

    johny

--- pag-empresa/editar.php
+++ pag-empresa/editar.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">  
 <link rel='stylesheet' href='../pag-empresa/css/style.css'>
-<title>TrampoTec</title>
+<title> TrampoTec </title>
 </head>
 <body>
     
