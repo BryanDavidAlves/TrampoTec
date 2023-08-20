0611ebb9ea997644596d8ba08aeaccc52f0d2999
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-08-19 21:30:17 -0300

    adicionando novo css em cada pagina

--- pag-aluno/notificacoes.php
+++ pag-aluno/notificacoes.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../reset.css">
-    <link rel="stylesheet" href="../pag-aluno/components/components-aluno.css">
+    <link rel="stylesheet" href="../pag-aluno/components/component-aluno.css">
     <link rel="stylesheet" href="../pag-aluno/css/notificacoes.css">
     <title>Document</title>
 </head>
