1593a94ea77ff05fd22d02fa1e98f7f6fb890561
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-08-19 23:16:16 -0300

    arrumando a tela de cadastro

--- pag-aluno/cadastro.php
+++ pag-aluno/cadastro.php
@@ -9,7 +9,7 @@
     <!--link icone filtro-->
     <link rel="stylesheet" href="../reset.css">
 
-    <link rel="stylesheet" href="../pag-aluno/css/login-de-cadastro.css">
+    <link rel="stylesheet" href="../pag-aluno/css/login-e-cadastro.css">
     <title>Processos Seletivos</title>
 </head>
 
@@ -23,40 +23,40 @@
                 <h1>Cadastro</h1>
                 <legend><i class="fa-solid fa-user" style="color: #ffffff;"></i></legend>
                 <br>
-                <br><br>
+                <br>
                 <div class="inputBox">
                     <i class="fa-solid fa-id-card" style="color: #ffffff;"></i>
                     <label class="labelInput" for="nome"> nome:</label>
                     <input type="text" id="nome" name="nome" class="inputUser" required>
                 </div>
-                <br><br>
+                <br>
                 <div class="inputBox">
                     <i class="fa-solid fa-id-card" style="color: #ffffff;"></i>
                     <label class="labelInput" for="cpf"> CPF:</label>
                     <input type="text" id="cpf" name="cpf" class="inputUser" required>
                 </div>
-                <br><br>
+                <br>
                 <div class="inputBox">
                     <i class="fa-solid fa-user" style="color: #ffffff;"></i>
                     <label class="labelInput" for="email"> email:</label>
                     <input type="text" id="email" name="email" class="inputUser" required>
 
                 </div>
-                <br><br>
+                <br>
                 <div class="inputBox">
                     <i class="fa-solid fa-lock" style="color: #ffffff;"></i>
                     <label class="labelInput" for="senha">Senha:</label>
                     <input type="password" id="senha" name="senha" class="inputUser" required>
 
                 </div>
-                <br><br>
+                <br>
                 <div class="inputBox">
                     <i class="fa-solid fa-lock" style="color: #ffffff;"></i>
-                    <label class="labelInput" for="Rsenha">Repita sua senha:</label>
-                    <input type="password" id="Rsenha" name="senha" class="inputUser" required>
+                    <label class="labelInput" for="Rsenha">Senha:</label>
+                    <input type="password" placeholder="Repita sua senha" id="Rsenha" name="Rsenha" class="inputUser" required>
 
                 </div>
-                <br><br>
+                <br>
                 <h2>Esqueceu sua senha?</h2>
                 <div class="botao">
                     <button class="btn">Criar</button>
