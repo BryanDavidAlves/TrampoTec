1a313a3851da85278a4977fc5d3e5374300d4a31
Johnydbr <djohny000@gmail.com>
2023-08-26 20:52:31 -0300

    estilizando side bar

--- pag-etec/componentes/sidebar.php
+++ pag-etec/componentes/sidebar.php
@@ -10,12 +10,12 @@
                     <li><a href="../pag-empresa/home.php"><i class="fa-solid fa-house"></i>Home</a></li>
                     <li><a href="../pag-empresa/vagas.php"><i class="fa-solid fa-bag-shopping"></i>Cursos</a></li>
                 </div>
-                <dvi>
+                <div>
                     <li onclick="clickmenu2()"><a><i class="fa-sharp fa-solid fa-bell fa-lg"></i>Notificações</a></li>
                     <li><a href="../pag-empresa/configuracoes.php"><i class="fa-solid fa-gear fa-lg"></i>Configurações</a></li>
                     <li> <a href="../pag-empresa/candidatos.php"><i class="fa-solid fa-user"></i> Perfil</a></li>
                     <li><a href="../beck-end/logoff/logoff.php"><i class="fa-solid fa-door-open"></i>Logout</a></li>
-                </dvi>
+                </div>
             </div>
         </ul>
     </nav>
