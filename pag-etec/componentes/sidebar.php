a57a57be578c06687fd1acafbceffdbe98a54958
Johnydbr <djohny000@gmail.com>
2023-08-25 23:18:54 -0300

    criando a pasta etec

---
+++ pag-etec/componentes/sidebar.php
@@ -0,0 +1,21 @@
+
+<header class="side-bar">
+        <nav>
+            <div class="imagem-empresa">
+               <a href="../pag-empresa/perfil.php"> <img src="img/nuts.png">
+                <i class="icon-foto fa-solid fa-caret-right"></i>
+                </a>
+            </div>
+            <ul>
+                <div class="coluna-um">
+                    <li><a href="../pag-empresa/home.php"><i class="fa-solid fa-house"></i>Home</a></li>
+                    <li> <a href="../pag-empresa/candidatos.php"><i class="fa-solid fa-user"></i>Candidatos</a></li>
+                    <li><a href="../pag-empresa/vagas.php"><i class="fa-solid fa-bag-shopping"></i>Vagas</a></li>
+                    <li onclick="clickmenu2()"><a><i class="fa-sharp fa-solid fa-bell fa-lg"></i>Notificações</a></li>
+                    <li onclick="clickmenu()"><a><i class="fa-solid fa-envelope"></i>Email</a></li>
+                    <li><a href="../pag-empresa/configuracoes.php"><i class="fa-solid fa-gear fa-lg"></i>Configurações</a></li>
+                    <li><a href="../beck-end/logoff/logoff.php"><i class="fa-solid fa-door-open"></i>Logout</a></li>           
+                </div>
+            </ul>
+        </nav>                 
+</header>  
---
+++ pag-etec/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./componentes/componente.css">
+    <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="../reset.css">
+    <title>TrampoTec</title>
+</head>
+<body>
+<?php include ('../pag-etec/componentes/sidebar.php')?>
+<?php include ('../pag-etec/componentes/filtro.php')?>
+</body>
+</html>
