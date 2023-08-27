0be24051406bdab0abffb312a181f09744be8ef6
Johnydbr <djohny000@gmail.com>
2023-08-26 22:50:46 -0300

    estilizando componentes

--- pag-etec/componentes/filtro.php
+++ pag-etec/componentes/filtro.php
@@ -1,6 +1,6 @@
 <div class="rightnav">
     <div class="itens"> <span> <i class="icon-notificacao fa-solid fa-sliders"></i> FILTRAR POR </span> </div>
-    <div class="itens"><a href=""> NOME </a>
+    <div class="itens"><a href=""> NOM DO CURSO </a>
         <select>
             <option value=""></option>
             <option value="">valor 1</option>
@@ -8,15 +8,24 @@
             <option value="">valor 3</option>
         </select>
     </div>
-    <div class="itens"><a href=""> CURSO </a>
-         <select>
+    <div class="itens"><a href=""> MODALIDADE </a>
+        <select>
             <option value=""></option>
             <option value="">valor 1</option>
             <option value="">valor 2</option>
             <option value="">valor 3</option>
         </select>
     </div>
-    <div class="itens"><a href=""> PALAVRA CHAVE </a>
+    <div class="itens"><a href=""> PERIODO </a>
+        <select>
+            <option value=""></option>
+            <option value="">valor 1</option>
+            <option value="">valor 2</option>
+            <option value="">valor 3</option>
+        </select>
+    </div>
+
+    <div class="itens"><a href=""> CARGA HORARIA </a>
         <select>
             <option value=""></option>
             <option value="">valor 1</option>
