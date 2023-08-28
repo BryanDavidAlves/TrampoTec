e356c363b99b19d0a0230effb4a8a94d4a4a9ec1
Beatriz Ferreira <bibika6541@gmail.com>
2023-08-28 22:31:40 +0000

    Ajustes na tela de cadastro de empresa e aluno e integração com viaCEP para auto complete de endereço

--- pag-aluno/cadastro.php
+++ pag-aluno/cadastro.php
@@ -2,139 +2,236 @@
 include('../dao/conexao.php');
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="css/cadastro.css">
-    <title>Resgistrar</title>
+    <link rel="stylesheet" href="../../reset.css">
+    <link rel='stylesheet' href='../../pag-empresa/css/login-cadastro.css'>
+    <link rel='stylesheet' href='../../../assets/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='../../../assets/css/style.css'>
+    <title>TrampoTec - Cadastro de Aluno </title>
 </head>
 
 <body>
-    <main>
-        <img class="cima" src="img/imagemfundocima.png" alt="">
-        <img class="baixo" src="img/imagemfundobaixo.png" alt="">
+    <img class="cima" src="img/imagemfundocima.png">
 
+    <img class="baixo" src="img/imagemfundobaixo.png">
 
-        <section class="cadastro">
-            <div class="box-formulario">
-                <section class="left">
-                    <img class="logo" src="img/trampotec-logo.png" alt="">
-                    <h2>SEJA BEM VINDO</h2>
-                    <img src="img/aluno-form.png" alt="">
-                </section>
-
-                <section class="right">
-                    <h2>INFORMAÇÕES DE CADASTRO</h2>
-
-                    <form action="./beck-end/cadastro/salvarCadastro.php" method="post">
-                        <div class="one-item">
-                            <label for="nome-aluno">NOME COMPLETO</label>
-                            <input type="text" name="nome-aluno" id="nome-aluno" required>
-
-                        </div>
-                        <div class="two-item">
-                            <div>
-                                <label for="nome-aluno">DATA DE NASCIMENTO</label>
-                                <input type="date" name="nasc-aluno" id="nasc-aluno" required>
-                            </div>
-                            <div>
-                                <label for="cpf-aluno">CPF</label>
-                                <input  maxlength="18" type="text" name="cpf-aluno" id="cpf-aluno"
-                                    placeholder="_ _ _ . _ _ _ . _ _ _ - _ _" required>
-                            </div>
-                        </div>
-
-                        <div class="one-item">
-                            <label for="nome-etec">INSTITUIÇÃO ETEC QUE VOCÊ ESTUDA</label>
-                            <div class="box-input">
-                                <input type="text" name="nome-etec" id="nome-etec" required>
-                            </div>
-                        </div>
-
-
-                        <div class="two-item">
-                            <div>
-                                <label for="tel-aluno">TELEFONE</label>
-                                <input  maxlength="11" type="text" name="tel-aluno" placeholder="() _ _ _ _ _ - _ _ _ _"
-                                    required>
-                            </div>
-                            <div style="margin-left:50px;">
-                                <label for="cep-aluno">CEP</label>
-                                <input  maxlength="9" type="text" name="cep-aluno" id="cep-aluno" placeholder="_ _ . _ _ _ - _ _ _"
-                                    required>
-                            </div>
-                        </div>
-
-                        <div class="one-item">
-                            <label for="senha-aluno">CRIAR SENHA</label>
-                            <div class="box-input">
-                                <input maxlength="8" type="password" name="senha-aluno" id="senha-aluno" required>
-                            </div>
-                        </div>
-                        <div class="one-item">
-                            <label for="repita-senha">REPITA A SENHA</label>
-                            <div class="box-input">
-                                <input maxlength="8" type="password" name="repita-senha" id="repita-senha" required>
-                            </div>
-                        </div>
-                        <?php
-                        if (isset($_GET['login']) && $_GET['login'] == "cpfInvalido") {
-                            ?>
-                            <div class="text-danger">
-                                CPF INVALIDO!!
-                            </div>
-                            <?php
-                        }
-                        ?>
-
-                        <?php
-                        if (isset($_GET['login']) && $_GET['login'] == "senhaFraca") {
-                            ?>
-                            <div class="text-danger">
-                                Senha fraca, obs: até 8 digitos, exemplo: Senha12@
-                            </div>
-                            <?php
-                        }
-                        ?>
-
-                        <?php
-                        if (isset($_GET['login']) && $_GET['login'] == "numeroInvalido") {
-                            ?>
-                            <div class="text-danger">
-                                Telefone Invalido
-                            </div>
-                            <?php
-                        }
-                        ?>
-                        <?php
-                        if (isset($_GET['login']) && $_GET['login'] == "senhasDiferentes") {
-                            ?>
-                            <div class="text-danger">
-                                Senhas Diferentes
-                            </div>
-                            <?php
-                        }
-                        ?>
-                        <input class="btn" type="submit" value="CADASTRAR">
-
-                    </form>
-                </section>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4 d-flex flex-column align-items-center justify-content-center">
+                <img class="logo" src="img/trampotec-logo.png" alt="foto do aluno">
+                <img width="80%" src="img/aluno-form.png">
             </div>
-        </section>
+            <div class="col-12 col-md-6">
+                <div class="row">
+                    <div class="col-12 mt-5">
+                            <p class="titulo-um"> Bem vindo! Crie sua conta</p>
+                            <p class="titulo-dois">Já tem uma conta? <a href="">Fazer login</a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <form id="form-cadastro" enctype="multipart/form-data" method="post" action="./beck-end/cadastro/salvarCadastro.php">
+                            <div class="row">
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Nome Completo</small></label>
+                                        <input type="text" class="form-control obrigatorio" id="nome" name="nome" placeholder="Nome Completo">
+                                        <div class="invalid-feedback">
+                                            O nome é obrigatório
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>CPF</small></label>
+                                        <input class="form-control obrigatorio cpf" type="text" placeholder="000.000.000-00" id="cpf-aluno" name="cdp-aluno">
+                                        <div class="invalid-feedback">
+                                            Digite um CPF válido
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small> Data de Nascimento</small></label>
+                                        <input type="date" class="form-control obrigatorio" id="nasc-aluno" name="nasc-aluno" placeholder="Data de Nascimento">
+                                        <div class="invalid-feedback">
+                                           Informe a data de nascimento
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small> Qual instituição você estuda?</small></label>
+                                        <input type="text" class="form-control obrigatorio" id="nome-etec" name="nome-etec" placeholder="Nome da Instituição">
+                                        <div class="invalid-feedback">
+                                           Informe a data de nascimento
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Email</small></label>
+                                        <input class="form-control obrigatorio" type="text" placeholder="exemplo@dominio.com" name="email">
+                                        <div class="invalid-feedback">
+                                            O email é obrigatório
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Telefone</small></label>
+                                        <input class="form-control celular obrigatorio" type="text" placeholder="(00) 90000-0000" name="telefone">
+                                        <div class="invalid-feedback">
+                                            O telefone é obrigatório
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Senha</small></label>
+                                        <input class="form-control obrigatorio senha" type="password" placeholder="********" id="senha" name="senha">
+                                        <div class="invalid-feedback">
+                                            A senha deve conter no mínimo 8 caracteres e conter pelo menos um número, uma letra maiúscula e um caracter especial
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Confirme a senha</small></label>
+                                        <input class="form-control obrigatorio confirm-password" type="password" placeholder="********" id="confirm-password" name="confirm-password">
+                                        <div class="invalid-feedback">
+                                            As senhas não são iguais
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-4">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>CEP</small></label>
+                                        <input class="form-control cep obrigatorio" type="numeric" placeholder="00000-000" name="cep">
+                                        <div class="invalid-feedback">
+                                            Informe um CEP válido
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Logradouro</small></label>
+                                        <input class="address-search form-control" readonly type="text" placeholder="Informe o CEP" id="logradouro" name="logradouro">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-2">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Número</small></label>
+                                        <input class="form-control obrigatorio" type="text" placeholder="Número" id="numero" name="numero">
+                                        <div class="invalid-feedback">
+                                            O número é obrigatório
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Bairro</small></label>
+                                        <input class="address-search form-control" readonly type="text" placeholder="Informe o CEP" id="bairro" name="bairro">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-4">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Cidade</small></label>
+                                        <input class="address-search form-control" readonly type="text" placeholder="Informe o CEP" id="cidade" name="cidade">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-2">
+                                    <div class="form-group">
+                                        <label class="font-weight-bold border-0" for="nome"><small>Estado</small></label>
+                                        <input class="address-search form-control" readonly type="text" placeholder="Informe o CEP" id="estado" name="estado">
+                                    </div>
+                                </div>
+                               
+                            </div>
+                            <div class="row">
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-submit-form mt-3 col-12">CADASTRAR</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="../../../assets/js/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="../../../assets/js/jquery.mask.min.js"></script>
+<script src="../../../assets/js/my-mask.js"></script>
+<script src="../../../assets/js/bootstrap.min.js"></script>
+<script src="../../../assets/js/main.js"></script>
 
+<script type="text/javascript">
+    $(function(){
+        $('input').keyup(function(){
+            $(this).removeClass('is-valid is-invalid');
+        });
 
-    </main>
-    <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
+        $('.cep').keyup(function(){
+            $(this).removeClass('is-valid is-invalid');
+            if(this.value.length == 9){
+                $.get(`https://viacep.com.br/ws/${this.value}/json/`, function(address){
+                    if(address.erro){
+                        $('.cep').removeClass('is-valid');
+                        $('.cep').addClass('is-invalid');
+                    }else{  
+                        $('.cep').removeClass('is-invalid');
+                        $('.cep').addClass('is-valid');
+                        $('#logradouro').val(address.logradouro);
+                        $('#bairro').val(address.bairro);
+                        $('#cidade').val(address.localidade);
+                        $('#estado').val(address.uf);
+                        $('#numero').focus();
+                    }
+                });
+            }else{
+                $('.address-search').val('');
+            }
+        });
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
+        $('#senha').keyup(function(){
+            if(!validatePassword(this.value)){
+                $(this).addClass('is-invalid');
+                $(this).removeClass('is-valid');
+            }else{
+                $(this).removeClass('is-invalid');
+                $(this).addClass('is-valid');
+            }
+            
+            if($('#confirm-password').val().length > 0) validateConfirmPassword(this.value, $('#confirm-password').val());
+            
+        });
 
-    <script type="text/javascript">
-        $("#tel-aluno, #celular").mask("(00) 0000-0000");
-    </script>
+        $('#confirm-password').keyup(function(){
+            if(this.value.length == 0) return false;
+            if($('#senha').val().length > 0) validateConfirmPassword($('#senha').val(), this.value);
+        });
+
+        $('.cpf').keyup(function(){
+            if(!validateCPF(this.value)){
+                $(this).addClass('is-invalid');
+                $(this).removeClass('is-valid');
+            }else{
+                $(this).removeClass('is-invalid');
+                $(this).addClass('is-valid');
+            }
+        });
+
+        $('#form-cadastro').submit(function(e){
+            if(validarForm($(this))){
+                e.preventDefault();
+            }
+        })
+    })
+</script>
 </body>
 
 </html>
