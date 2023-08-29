d43570973d97c4f059f67bee1bcfdf63722dbb20
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-08-29 16:40:01 -0300

    tabela de professor e empresas

--- pag-adm/empresa.php
+++ pag-adm/empresa.php
@@ -9,6 +9,7 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <!--link icone filtro-->
     <link rel="stylesheet" href="../reset.css">
+
     <link rel="stylesheet" href="components/component-adm.css">
     <link rel="stylesheet" href="css/empresa.css">
 
@@ -73,7 +74,65 @@
         </section>
         <section class="empresa">
 
-
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NOME</th>
+                        <th>EMAIL</th>
+                        <th>CNPJ</th>
+                        <th>CEP</th>
+                        <th>LOGRADOURO</th>
+                        <th>NÚMERO</th>
+                        <th>BAIRRO</th>
+                        <th>ESTADO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="infos">
+                        <td class="table-id">1</td>
+                        <td class="table-nome-empresa">Cysco Systen</td>
+                        <td class="table-email-empresa">cysco@gmail.com</td>
+                        <td class="table-cnpj">832959432432</td>
+                        <td class="table-cep">63833</td>
+                        <td class="table-log">kjwajvfew</td>
+                        <td class="table-numero">312</td>
+                        <td class="table-bairro">ffewrwfew</td>
+                        <td class="table-estado">São Paulo</td>
+                        <td class="icone-table"><i class="fa-solid fa-check" style="color: #ff0000;"></i> <i
+                                class="fa-solid fa-x" style="color: #000000;"></i>
+                        </td>
+                    </tr>
+                    <tr class="infos">
+                        <td class="table-id">2</td>
+                        <td class="table-nome-empresa">Cysco Systen</td>
+                        <td class="table-email-empresa">cysco@gmail.com</td>
+                        <td class="table-cnpj">832959432432</td>
+                        <td class="table-cep">63833</td>
+                        <td class="table-log">kjwajvfew</td>
+                        <td class="table-numero">312</td>
+                        <td class="table-bairro">ffewrwfew</td>
+                        <td class="table-estado">São Paulo</td>
+                        <td class="icone-table"><i class="fa-solid fa-check" style="color: #ff0000;"></i> <i
+                                class="fa-solid fa-x" style="color: #000000;"></i>
+                        </td>
+                    </tr>
+                    <tr class="infos">
+                        <td class="table-id">2</td>
+                        <td class="table-nome-empresa">Cysco Systen</td>
+                        <td class="table-email-empresa">cysco@gmail.com</td>
+                        <td class="table-cnpj">832959432432</td>
+                        <td class="table-cep">63833</td>
+                        <td class="table-log">kjwajvfew</td>
+                        <td class="table-numero">312</td>
+                        <td class="table-bairro">ffewrwfew</td>
+                        <td class="table-estado">São Paulo</td>
+                        <td class="icone-table"><i class="fa-solid fa-check" style="color: #ff0000;"></i> <i
+                                class="fa-solid fa-x" style="color: #000000;"></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
         </section>
     </main>
