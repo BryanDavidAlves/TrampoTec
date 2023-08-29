b23e47fa2927325fccfac4eb4eb95dbab233bff1
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-08-28 23:32:48 -0300

    adicionando tela de cadastro de curso

--- pag-etec/componentes/sidebar.php
+++ pag-etec/componentes/sidebar.php
@@ -7,14 +7,13 @@
         <ul>
             <div class="coluna-um">
                 <div>
-                    <li><a href="../pag-empresa/home.php"><i class="fa-solid fa-house"></i>Home</a></li>
-                    <li><a href="../pag-empresa/vagas.php"><i class="fa-solid fa-bag-shopping"></i>Cursos</a></li>
+                    <li><a href="index.php"><i class="fa-solid fa-house"></i>Home</a></li>
                 </div>
                 <div>
-                    <li onclick="clickmenu2()"><a><i class="fa-sharp fa-solid fa-bell fa-lg"></i>Notificações</a></li>
-                    <li><a href=""><i class="fa-solid fa-gear fa-lg"></i>Configurações</a></li>
-                    <li> <a href=""><i class="fa-solid fa-user"></i> Perfil</a></li>
-                    <li><a href=""><i class="fa-solid fa-door-open"></i>Logout</a></li>
+                    <li onclick="clickmenu()"><a><i class="fa-sharp fa-solid fa-bell fa-lg"></i>Notificações</a></li>
+                    <li><a href="configuracoes.php"><i class="fa-solid fa-gear fa-lg"></i>Configurações</a></li>
+                    <li> <a href="perfil.php"><i class="fa-solid fa-user"></i> Perfil</a></li>
+                    <li><a href="./pags-login-etec/login.php"><i class="fa-solid fa-door-open"></i>Logout</a></li>
                 </div>
             </div>
         </ul>
