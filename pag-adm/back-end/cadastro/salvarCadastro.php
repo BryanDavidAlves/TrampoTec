ad188853cd2ccc65fc6ddd51ce02a7cff23abdb6
0RyanSouza0 <tlousouza678@gmail.com>
2023-08-31 11:02:42 -0300

    Adicionando delete no adm

--- pag-adm/back-end/cadastro/salvarCadastro.php
+++ pag-adm/back-end/cadastro/salvarCadastro.php
@@ -1,1 +1,28 @@
+<?php
+include '../../../dao/conexao.php';
 
+//VERIFICA SE ESTÁ VINDO INFORMAÇÕES VIA POST
+if ($_POST) {
+    //passando todos os itens do post para as sua variaveis
+    $nome = trim($_POST['nome']);
+    $email = trim($_POST['email']);
+    $senha = trim($_POST['senha']);
+
+    $sql = "
+                INSERT INTO tb_admin (nome , email , senha ) VALUES
+                (   '$nome',
+                    '$email',
+                    '$senha'
+
+                )
+                ";
+    $query = $conexao->prepare($sql);
+    $query->execute();
+    $id = $conexao->lastInsertId();
+
+    header('Location:../../adm.php');
+    exit;
+} else {
+    header('Location: login.php?login=erro');
+    $_SESSION['autenticado'] = "NAO";
+}
