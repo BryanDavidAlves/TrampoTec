3690b2197ce3880903fcb8da5a574806ccebd479
Rafael Landim <rafaellandim2016@gmail.com>
2023-09-17 20:51:54 -0300

    Atualização da confirmação do aluno com validação do email etec, e feita a confirmação do email do professor com validação etec

--- pag-professor/confirmar_email.php
+++ pag-professor/confirmar_email.php
@@ -20,28 +20,31 @@
                 <h1>Confirmação de Email</h1>
                 <p>Valide seu email para prosseguir com o cadastro</p>
                 </div>
-                <form action="login.php">
+                <form class="send-validation-email" id="form-valida-email" action="../helpers/confirm-mail-helper.php">
+                    <input type="hidden" name="template" value="../templates/confirm-your-mail-professor.html">
                     <div class="item-form">
-                        <label for="nome-professor">EMAIL</label>
+                        <label for="email">EMAIL</label>
                         <div class="div-input">
-                            <input type="email" name="email-professor" placeholder="Digite seu email" id="email-professor">
+                            <input class="obrigatorio email" type="text" name="email" placeholder="Digite seu email" id="email">
+                            <small class="invalid-feedback">Digite um email válido</small>
                         </div>
                     </div>
                     <div class="item-form">
-                        <label for="email-institucional">CONFIRMAR EMAIL </label>
+                        <label for="email-confirmation">CONFIRMAR EMAIL </label>
                         <div class="div-input">
-                            <input type="email" name="email-institucional" id="email-institucional" placeholder="Confirme seu email">
+                            <input class="obrigatorio email-confirmation" type="text" name="email-confirmation" id="email-confirmation" placeholder="Confirme seu email">
+                            <small class="invalid-feedback">Os e-mails não conferem</small>
                         </div>
                     </div>
-                    
                     <input class="botao" type="submit" value="Enviar">
-
-
                 </form>
             </div>
         </section>
     </main>
     <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
+    <script src="../assets/js/jquery-3.7.1.js"></script>
+    <script src="../assets/js/sweetalert2@11.js"></script>
+    <script src="../assets/js/main.js"></script>
 </body>
 
 </html>
