f9fa695092a87bb18d6d23c82c6b044bea2c5d92
0RyanSouza0 <tlousouza678@gmail.com>
2023-10-01 14:56:48 -0300

    parte cadastro atualizada

--- pag-professor/back-end/login/validador_acesso.php
+++ pag-professor/back-end/login/validador_acesso.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] == 'não'){
+if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] == 'não') {
 
-    header ('Location: login.php?cadastro=inexistente');
+    header('Location: cadastro.php?cadastro=inexistente');
 }
-
-?>
