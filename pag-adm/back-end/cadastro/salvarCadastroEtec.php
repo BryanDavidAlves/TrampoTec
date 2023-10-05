ce6912930af69ff5229e9a3d22186daa0994fb55
LaysaMelmudes <laysamelmudes@gmail.com>
2023-10-05 19:44:55 -0300

    cadastro vaga quase finalizado

--- pag-adm/back-end/cadastro/salvarCadastroEtec.php
+++ pag-adm/back-end/cadastro/salvarCadastroEtec.php
@@ -9,16 +9,11 @@ if ($_POST) {
     $email = trim($_POST['email']);
     $codigo = trim($_POST['codigo']);
     $municipio = trim($_POST['municipio']);
-    $curso = trim($_POST['curso']);
+   
 
+    
 
-
-
-
-
-
-    $sql1 = "
-                INSERT INTO tb_etec ( nome , email , codigo , municipio ) VALUES
+    $sql = " INSERT INTO tb_etec ( nome , email , codigo , municipio ) VALUES
                 (   '$nome',
                     '$email',       
                     '$codigo',
@@ -26,20 +21,27 @@ if ($_POST) {
 
                 )
                 ";
-    $query = $conexao->prepare($sql1);
+    $query = $conexao->prepare($sql);
     $query->execute();
     $id = $conexao->lastInsertId();
-
+    
   
-        $sql1 = " INSERT INTO tb_curso_etec ( fk_idCurso , fk_idEtec ) VALUES
-        (   '$curso',
-            '$id'
-        )
-        ";
+    foreach ($_POST["selectName"] as $valor) {
+
+        $select = $_POST["selectName"];
+        $querySelect = "SELECT * FROM  tb_curso WHERE nome = $select";
+
+        $query = $conexao->query($querySelect);
+
+        $curso = $query->fetchAll();
+        $cursoId = $curso[0];
+        // Inserir o valor no banco de dados
+        $sql = "INSERT INTO tb_curso_etec ( fk_idCurso , fk_idEtec) VALUES ('$cursoId', $id)"; 
+        $query = $conexao->prepare($sql);
+        $query->execute();// Substitua 'tabela' e 'coluna' com os nomes apropriados
+     } 
     
-$query = $conexao->prepare($sql1);
-$query->execute();
-    
+
 
     header('Location:../../cadastro-etec.php?cadastro=feito');
     exit;
