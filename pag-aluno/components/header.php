39ec8df306e82919917b7744ed0aa957715e0f9d
unknown <giulliameneses@gmail.com>
2023-10-19 21:39:27 -0300

    home

--- pag-aluno/components/header.php
+++ pag-aluno/components/header.php
@@ -1,44 +1,101 @@
     <header>
+        <style>
+            *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Nunito', sans-serif;
+}
 
+a{
+    text-decoration: none;
+}
+
+ul{
+    list-style: none;
+}
+
+.header{
+    width: 100%;
+    height: 30%;
+    background-color: #fff;
+    position: relative;    
+}
+
+.header .navbar{
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+
+}
+
+.logo img{
+    margin-top: 2%;
+    width: 280px;
+    height: 100px;
+    margin-left: 27%;
+
+}
+
+.menu ul li{
+    display: inline-block;
+    padding: 0 50px;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 80px;
+    align-items: center;
+    color: black;
+}
+
+.menu{
+    margin-left: 4%;
+    margin-top: 2%;
+}
+
+.login-btn{
+    font-size: 19px;
+    font-weight: 700;
+    border-radius: 30px;
+    background-color: #f3ef51;
+    padding: 8px 40px;
+    margin-right: 13%;
+    color: black;
+}
+
+        </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-         <div class="container-fluid">
-            <img src="../pag-aluno/img/trampotec-logo.png" alt="">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-    </div>
-  </div>
-</nav>
+    
 
+<div class="header">
+     <div class="navbar">
+         <div class="logo">
+             <a href="#"><img src="../pag-aluno/img/trampotec-logo.png"></a>
+         </div>
+         <div class="menu">
+             <ul>
+         <a href="index.php"><li>Home</li></a>
+         <a href="processos-seletivos.php"><li>Pocesso seletivo</li>
+         <a href="curriculo.php"> <li>Vagas</li>
+         <a href="curriculo.php"> <li>Recomendações</li>
+         <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Meu Perfil
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+         </div>
+         <!-- <div class="login-btn">Notificações</div>
+     </div>    -->
     </header>
 
--- pag-aluno/index.php
+++ pag-aluno/index.php
@@ -8,60 +8,139 @@ require_once "./back-end/login/validador_acesso.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <style>
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <!--link icone filtro-->
+             
+      .banner{
+          width: 80%;
+          height: 70%;
+          position: absolute;
+
+          left: 150px;
+          color: #fff;
+      }
+      
+      .app-text{
+          width: 50%;
+          float: left;
+          padding-left: 50px;
+      }
+      
+      .app-text h3{
+          margin-top: 100px;
+          font-size: 19px;
+          color: #000;
+          font-weight: 400;
+          letter-spacing: 2px;
+          text-transform: uppercase;
+      }
+      
+      .app-text h1{
+          font-size: 51px;
+          position: relative;
+          color: #000;
+          margin-top: 30px;
+          font-weight: 700;
+          line-height: 1;
+      }
+      
+      .app-text p{
+        margin-top: 6%;
+          font-size: 20px;
+          margin: 30px 30px 30px 0;
+          letter-spacing: 1px;
+          color: #000;
+          padding-right: 250px;
+      }
+      
+      .btn-group{
+          display: flex;
+          width: 100%;
+          position: relative;
+      }
+      
+      .btn-group .signup-btn{
+          cursor: pointer;
+          display: block;
+          color: #000;
+          text-align: center;
+          font-size: 15px;
+          font-weight: 700;
+          border-radius: 32px;
+          background-color: #ffa037;
+          padding: 15px 50px;
+      }
+      
+      .btn-group .play-btn{
+          margin-left: 40px;
+          display: inline-flex;
+      }
+      
+      .btn-group span{
+          color: #000;
+          font-size: 15px;
+          font-weight: 700;
+          margin: auto 20px;
+          cursor: pointer;
+      }
+      
+      .play-btn-inner{
+          height: 50px;
+          width: 50px;
+          border: 2px solid transparent;
+          border-radius: 50%;
+          background-color: #000;
+      }
+      
+      .play-btn-inner i{
+          padding: 15px 19px;
+          font-size: 16px;
+          cursor: pointer;
+      }
+
+    </style>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../reset.css">
     <link rel="stylesheet" href="../pag-aluno/components/components-aluno.css">
     <link rel="stylesheet" href="../pag-aluno/css/home.css">
 
-    <title>Pagina de Vagas</title>
+    <title>Pagina de Vagas</title>    
+    <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
+    <script src="js/modal-vagas.js"></script> 
 </head>
 
 <body>
-    <img class="cima-esquerda" src="img/icon4.png" alt="">
-    <img class="cima-direita" src="img/icon2.png" alt="">
-    <!--
-    <img class="baixo-direita" src="img/icon1.png" alt="">
-    <img class="baixo-esquerda" src="img/icon5.png" alt="">
-    -->
+
+    
     <?php
     include('../pag-aluno/components/header.php');
     ?>
-    <main id="main">
-
-        <div class="box">
-            <h1> </h1>
-
-            <section class="inicio">
-                <div class="left">
-                    <h2>VAGAS DE ESTÁGIO, <br>
-                        INGRESSE NO MERCADO <br>
-                        DE TRABALHO!
-                    </h2>
-                    <span>
-                        <a href="curriculo.php">CURRÍCULO</a>
-                        <a href="painel-de-vagas.php">VAGAS</a>
-                    </span>
-                </div>
-                <div class="right">
-                    <img src="img/img-home.png" alt="">
-                </div>
-            </section>
-
-        </div>
-
-    </main>
     <?php
     include('../pag-aluno/components/sidebar.php');
     ?>
 
-    <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
-    <script src="js/modal-vagas.js"></script>
+    <div class="banner">
+         <div class="app-text">
+             <h3>Bem Vindo(a) ao TrampoTec</h3>
+             <h1>Ingressando você<br>no mercado de trabalho </h1>
+             <p>Da sala de aula para o mercado de trabalho, de maneira facíl, rápida e segura.</p>
 
+             <div class="btn-group">
+                 <div class="signup-btn">Crie seu currículo</div>
+                 <div class="play-btn">
+                     <div class="play-btn-inner">
+                         <i class="fa fa-play"></i>
+                     </div>
+                     <span>Vagas</span>
+                 </div>
+             </div>
+         </div>
+         <div class="app-picture">
+             <img src="img/img-home.png">
+         </div>
+     </div>
 </body>
 
 </html>
