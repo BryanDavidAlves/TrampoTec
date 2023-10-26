74e36eb8ae0b3cd98bf2e989c73e020083189b52
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-10-25 23:33:33 -0300

    estilizando o formulario 3

--- pag-aluno/formulario3.php
+++ pag-aluno/formulario3.php
@@ -1,7 +1,8 @@
 <?php
 require_once "./back-end/login/validador_acesso.php";
-include '../dao/conexao.php';
+include('../dao/conexao.php');
 $aluno_id = $_SESSION['idAluno'];
+
 
 ?>
 <?php
@@ -16,13 +17,16 @@ require_once "./back-end/login/validador_acesso.php";
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
     <!--link icone filtro-->
     <link rel='stylesheet' href='../assets/css/bootstrap.min.css'>
     <link rel="stylesheet" href="../reset.css">
     <link rel="stylesheet" href="../pag-aluno/components/components-aluno.css">
-    <link rel="stylesheet" href="../pag-aluno/css/curriculo.css">
+    
 
     <title>Meu Curriculo</title>
 
@@ -31,20 +35,29 @@ require_once "./back-end/login/validador_acesso.php";
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         }
 
-        .container {
-            align-items: center;
+        .align-tudo {
+            position: fixed;
+            width: 100%;
+            height: 100%;
             display: flex;
-            justify-content: space-between;
-            width: 50%;
+            align-items: center;
+            justify-content: center;
+            margin-top: 2%;
+            
+
         }
 
         .form-container {
-            width: 40%;
-            padding: 20px;
-            background-color: #f0f0f0;
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            margin: 10px;
+            align-items: center;
+            display: flex;
+            justify-content: space-between;
+            width: 400px;
+            box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+            display: flex;
+            flex-direction: column;
+            border-radius: 20px;
+            height: 480px;
+            padding: 10px;
         }
 
 
@@ -53,7 +66,8 @@ require_once "./back-end/login/validador_acesso.php";
             margin-top: 10px;
         }
 
-        input, select {
+        input,
+        select {
             width: 100%;
             padding: 10px;
             margin-top: 5px;
@@ -74,19 +88,45 @@ require_once "./back-end/login/validador_acesso.php";
         input[type="submit"]:hover {
             background-color: #45a049;
         }
-        .tela{
+
+        .tela {
             border-radius: 30px;
             height: 290px;
-            width: 250px;
+            width: 300px;
+            background-color: rgba(247, 247, 247, 0.641);
         }
-        th{
+
+        th {
             text-align: center;
             font-weight: bold;
             font-size: 15px;
         }
-        td{
+
+        td {
             text-align: center;
             font-size: 15px;
+        }
+
+        .container {
+
+            display: flex;
+            flex-direction: column;
+        }
+
+        .button {
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+            width: 300px;
+        }
+
+        .align-habilidade {
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+            padding: 10px;
+            height: 115px;
+            overflow: auto;
         }
     </style>
 
@@ -94,103 +134,96 @@ require_once "./back-end/login/validador_acesso.php";
 
 <body>
     <?php
-include '../pag-aluno/components/header.php';
-?>
-<div class="container">
-    <div class="formulario">
-        <form action="back-end/cadastro/salvarCurriculoConhecimento.php" id="knowledge-form" method="POST">
+    include('../pag-aluno/components/header.php');
+    ?>
+    <div class="align-tudo">
+        <div class="form-container">
+            <div class="formulario">
+                <form action="back-end/cadastro/salvarCurriculoConhecimento.php" id="knowledge-form" method="POST">
 
-        <label for="conhecimentos">Conhecimentos:</label>
-                <input type="text" id="conhecimentos" name="conhecimentos" placeholder="Digite um conhecimento">
-                <button type="submit" class="button">Adicionar Novo Conhecimento</button>
-                <br>
-                <label for="habilidades">Habilidades:</label>
-                <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-
-
-
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="4">
-                <label class="" for="">Trabalho em equipe</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="1">
-                <label class=" " for="">Comunicação</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="3">
-                <label class=" " for="">Empatia</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="8">
-                <label class=" " for="">Criatividade</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="10">
-                <label class=" " for="">Autoconfiança</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="13">
-                <label class=" " for="">Autocontrole</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="11">
-                <label class=""></label>
-
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="15">
-                <label class="" for="">Organização</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="14">
-                <label class="" for="">Tolerância</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="12">
-                <label class="" for="">Pensamento Analítico</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="7">
-                <label class="" for="">Adaptabilidade</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="5">
-                <label class="" for="">Pensamento Crítico</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="6">
-                <label class="" for="">Resolução de Conflitos</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="2">
-                <label class="" for="">Inteligência Emocional</label>
-                <input type="checkbox" class="" id="" autocomplete="off" name="habilidades[]" value="9">
-                <label class="btn btn-outline-primary" for="">Resiliência</label>
+                    <label for="conhecimentos">Conhecimentos:</label>
+                    <input type="text" id="conhecimentos" name="conhecimentos" placeholder="Digite um conhecimento">
+                    <button type="submit" class="button">Adicionar Novo Conhecimento</button>
+                    <br>
+                    <label for="habilidades">Habilidades:</label>
+                    <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
 
 
+                        <div class="align-habilidade">
+                            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btncheck1">Trabalho em equipe</label>
+
+                            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btncheck1">Trabalho em equipe</label>
+
+                            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btncheck1">Trabalho em equipe</label>
+
+                            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btncheck1">Trabalho em equipe</label>
+
+                            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btncheck1">Trabalho em equipe</label>
+
+                            <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off">
+                            <label class="btn btn-outline-primary" for="btncheck1">Trabalho em equipe</label>
+                        </div>
 
 
-                </div>
-                <br>
-        <input type="submit" value="Finalizar">
-        </form>
-    </div>
+                    </div>
+                    <br>
+                    <input type="submit" value="Finalizar">
+                </form>
+            </div>
 
-        <div class="tela">
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Conhecimentos selecionados:</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+            <div class="tela">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Conhecimentos selecionados:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
 
-$querySelect = "SELECT * FROM  tb_conhecimento_aluno WHERE fk_idAluno = $aluno_id ";
+                        $querySelect = "SELECT * FROM  tb_conhecimento_aluno WHERE fk_idAluno = $aluno_id ";
 
-$query = $conexao->query($querySelect);
+                        $query = $conexao->query($querySelect);
 
-$conhecimentoId = $query->fetchAll();
-
-foreach ($conhecimentoId as $conhecimentoId) {
-
-    $idCon = $conhecimentoId[0];
-
-    $querySelect = "SELECT * FROM  tb_conhecimento WHERE idConhecimento = $idCon  ";
-
-    $query = $conexao->query($querySelect);
-
-    $nome = $query->fetchAll();
-
-    foreach ($nome as $nome) {
-        ?>
-    <tr>
-      <td><?=$nome[1]?></td>
-      <td class="icone-table"> <a href="back-end/crudConhecimento/conhecimento-delete.php?id=<?=$nome[0]?>"><i class="fa-solid fa-x" style="color: #000000;"></i></a>
-
-                            </td>
+                        $conhecimentoId = $query->fetchAll();
 
 
-    </tr>
-    <?php }}?>
-  </tbody>
-</table>
+
+                        foreach ($conhecimentoId as $conhecimentoId) {
+
+                            $idCon = $conhecimentoId[0];
+
+                            $querySelect = "SELECT * FROM  tb_conhecimento WHERE idConhecimento = $idCon  ";
+
+                            $query = $conexao->query($querySelect);
+
+                            $nome = $query->fetchAll();
+
+                            foreach ($nome as $nome) {
+                                ?>
+                                <tr>
+                                    <td>
+                                        <?= $nome[1] ?>
+                                    </td>
+                                    <td class="icone-table"> <a
+                                            href="back-end/crudConhecimento/conhecimento-delete.php?id=<?= $nome[0] ?>"><i
+                                                class="fa-solid fa-x" style="color: #000000;"></i></a>
+
+                                    </td>
+
+
+                                </tr>
+                            <?php }
+                        } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-
     </div>
 
 
