892c5b4b3479a2fbb52f20c229b3a4765f762f71
unknown <giulliameneses@gmail.com>
2023-11-09 21:25:40 -0300

    .....

--- pag-aluno/formulario3.php
+++ pag-aluno/formulario3.php
@@ -15,6 +15,8 @@ require_once "./back-end/login/validador_acesso.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
@@ -23,27 +25,26 @@ require_once "./back-end/login/validador_acesso.php";
     <!--link icone filtro-->
     <link rel='stylesheet' href='../assets/css/bootstrap.min.css'>
     <link rel="stylesheet" href="../reset.css">
-  
+    <link rel="stylesheet" href="../pag-aluno/components/components-aluno.css">
+
+
     <title>Meu Curriculo</title>
 
     <style>
-      body {
-        font-family: sans-serif;
-      }
-      .link {
-  margin-top: 5%;
-    width: 300px;
-  background-color: #4CAF50;
-  color: white;
-  padding: 10px 15px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
+        body {
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+
         .align-tudo {
+            position: fixed;
+            width: 100%;
+            height: 100%;
             display: flex;
             align-items: center;
             justify-content: center;
+            margin-top: 2%;
+
+
         }
 
         button {
@@ -55,7 +56,6 @@ require_once "./back-end/login/validador_acesso.php";
             font-weight: 600;
             color: white;
             transition: 0.2s all ease-in-out;
-            
         }
 
         button:hover {
@@ -73,17 +73,16 @@ require_once "./back-end/login/validador_acesso.php";
             display: flex;
             flex-direction: column;
             border-radius: 20px;
-            height: 650px;
+            height: 480px;
             padding: 10px;
-            margin-top: 5%;
         }
+
 
         label {
             display: block;
-            margin-top: 20px;
-            font-weight: bold;
-            font-size: medium;
+            margin-top: 10px;
         }
+
         input,
         select {
             width: 100%;
@@ -95,34 +94,25 @@ require_once "./back-end/login/validador_acesso.php";
         }
 
         input[type="submit"] {
-        padding: 11px;
-        border-radius: 15px;
-        border: 1px solid #0a3580;
-        background-color: #0a3580;
-        width: 200px;
-        font-weight: 600;
-        color: white;
-        transition: 0.2s all ease-in-out;
-        margin-left: auto;
-        margin-right: auto;
-        display: block;
-        width: 100%;
-        border-radius: 5px;
-        text-decoration: none;
-        text-align: center;
-}
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
 
         input[type="submit"]:hover {
-        background-color: white;
-        color: #0a3580;
-        transition: 0.2s all ease-in-out;
-}
-.tela {
-  background-color: red;
-  border-radius: 4px;
-  height: 10%;
-  width: 30%;
-}
+            background-color: #45a049;
+        }
+
+        .tela {
+            border-radius: 30px;
+            height: 290px;
+            width: 300px;
+            background-color: rgba(247, 247, 247, 0.641);
+            overflow-y: auto;
+        }
 
         th {
             text-align: center;
@@ -156,16 +146,6 @@ require_once "./back-end/login/validador_acesso.php";
             height: 115px;
             overflow: auto;
         }
-
-        input[type="text"] {
-  margin-top: 5%;
-  background-color: white;
-  padding: 10px 15px;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-
     </style>
 
 </head>
@@ -174,11 +154,8 @@ require_once "./back-end/login/validador_acesso.php";
 
     <?php
     include('../pag-aluno/components/header.php');
+    include('../pag-aluno/components/sidebar.php');
     ?>
-<div class="progress">
-  <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-</div>
-
     <div class="align-tudo">
         <div class="form-container">
             <div class="formulario">
@@ -186,7 +163,7 @@ require_once "./back-end/login/validador_acesso.php";
 
                     <label for="conhecimentos">Conhecimentos:</label>
                     <input type="text" id="conhecimentos" name="conhecimentos" placeholder="Digite um conhecimento">
-                    <button type="submit" class="link">Adicionar Novo Conhecimento</button>
+                    <button type="submit" class="button">Adicionar Novo Conhecimento</button>
                 </form>
                 <br>
                 <form action="back-end/cadastro/salvarCurriculoHabilidade.php" method="post">
@@ -319,4 +296,3 @@ require_once "./back-end/login/validador_acesso.php";
 </html>
 <script src="js/funcoes.js"></script>
 <script src="https://kit.fontawesome.com/57efc2ce52.js" crossorigin="anonymous"></script>
-
