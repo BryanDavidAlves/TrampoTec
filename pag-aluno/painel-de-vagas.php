012bf233ce16e0fbef5bb7051ce8fa453764fc3e
unknown <giulliameneses@gmail.com>
2023-11-11 21:19:08 -0300

    ,

--- pag-aluno/painel-de-vagas.php
+++ pag-aluno/painel-de-vagas.php
@@ -314,7 +314,6 @@ include '../pag-aluno/components/header.php';
     <script>
         var btn = document.getElementById('btn')
 
-
         var button1 = document.getElementById("btn1")
         var button2 = document.getElementById("btn2")
         var button3 = document.getElementById("btn3")
