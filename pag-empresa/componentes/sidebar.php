6e3f8c7e7556446d55cd4d9f384348e6f1b20a0f
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-11-13 23:27:14 -0300

    padronizando a sidebar

--- pag-empresa/componentes/sidebar.php
+++ pag-empresa/componentes/sidebar.php
@@ -14,26 +14,29 @@ $resultados = $query->fetchAll();
 
 ?>
 <header class="side-bar">
-        <nav>
-            <div class="imagem-logo">
-                    <img src="img/trampotec.png" alt="">
-            </div>
-            <div class="imagem-empresa">
-               <a href="../pag-empresa/perfil.php"><?php foreach ($resultados as $resultados) {?> <img src="fotosEmpresa/perfil/<?=$resultados[13]?>"> <?php }?>
+    <nav>
+        <div class="imagem-logo">
+            <img src="img/trampotec.png" alt="">
+        </div>
+        <div class="imagem-empresa">
+            <a href="../pag-empresa/perfil.php">
+                <?php foreach ($resultados as $resultados) { ?> <img src="fotosEmpresa/perfil/<?= $resultados[13] ?>">
+                <?php } ?>
                 <i class="icon-foto fa-solid fa-caret-right"></i>
-                </a>
+            </a>
+        </div>
+        <ul>
+            <div class="coluna-um">
+                <li><a href="../pag-empresa/dash.php"><i class="fa-solid fa-house"></i> Dashboard</a></li>
+                <li><a href="../pag-empresa/home.php"><i class="fa-solid fa-house"></i> Home</a></li>
+                <li> <a href="../pag-empresa/candidatos.php"><i class="fa-solid fa-user"></i> Candidatos</a></li>
+                <li><a href="../pag-empresa/vagas.php"><i class="fa-solid fa-bag-shopping"></i> Vagas</a></li>
+                <li onclick="clickmenu2()"><a><i class="fa-sharp fa-solid fa-bell fa-lg"></i> Notificações</a></li>
+                <li onclick="clickmenu()"><a><i class="fa-solid fa-envelope"></i> Email</a></li>
+                <li><a href="../pag-empresa/configuracoes.php"><i class="fa-solid fa-gear fa-lg"></i> Configurações</a>
+                </li>
+                <li><a href="./beck-end/login/logout.php"><i class="fa-solid fa-door-open"></i> Logout</a></li>
             </div>
-            <ul>
-                <div class="coluna-um">
-                <li><a href="../pag-empresa/dash.php"><i class="fa-solid fa-house"></i>Dashboard</a></li>
-                    <li><a href="../pag-empresa/home.php"><i class="fa-solid fa-house"></i>Home</a></li>
-                    <li> <a href="../pag-empresa/candidatos.php"><i class="fa-solid fa-user"></i>Candidatos</a></li>
-                    <li><a href="../pag-empresa/vagas.php"><i class="fa-solid fa-bag-shopping"></i>Vagas</a></li>
-                    <li onclick="clickmenu2()"><a><i class="fa-sharp fa-solid fa-bell fa-lg"></i>Notificações</a></li>
-                    <li onclick="clickmenu()"><a><i class="fa-solid fa-envelope"></i>Email</a></li>
-                    <li><a href="../pag-empresa/configuracoes.php"><i class="fa-solid fa-gear fa-lg"></i>Configurações</a></li>
-                    <li><a href="./beck-end/login/logout.php"><i class="fa-solid fa-door-open"></i>Logout</a></li>
-                </div>
-            </ul>
-        </nav>
+        </ul>
+    </nav>
 </header>
